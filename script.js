b6ed90b36849547f3996c945349c8b4664d01026
ednnaa06 <edna.divjan@cetvrta-gimnazija.edu.ba>
2026-04-05 23:52:23 +0200

    Add files via upload
    Završna verzija projekta u Visual Studio Code programu. Projekat se sastoji od .html, .css i .js fajlova, te folder sa slikama koje su bile potrebne za implementaciju web stranice.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const navLinks=document.querySelectorAll(".nav-menu .nav-link");
 const menuOpenButton=document.querySelector("#menu-open-button");
 const menuCloseButton=document.querySelector("#menu-close-button");
 
@@ -5,4 +6,45 @@ menuOpenButton.addEventListener("click", ()=> {
     document.body.classList.toggle("show-mobile-menu");
 });
 
+//Close button kada je kliknut close button
 menuCloseButton.addEventListener("click", ()=> menuOpenButton.click());
+
+//Close button kada je kliknut nav link 
+navLinks .forEach(link => {
+    link.addEventListener("click", () => menuOpenButton.click());
+});
+
+//inicijalizacija Swipera
+const swiper = new Swiper('.slider-container', {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 25,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  //Responsive breakpoints
+  /*breakpoints: {
+    0: {
+        slidesPerView: 1
+    },
+    768: {
+        slidesPerView: 2
+    },
+    1024: {
+        slidesPerView: 3
+    }
+  },*/
+    slidesPerView: 1,
+    watchOverflow: false
+});
